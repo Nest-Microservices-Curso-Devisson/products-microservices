001c6588e8fb0e66fcba0ce03bf1918fc546fab1
Devisson Vasquez <devissonvasquez99@gmail.com>
2024-07-15 19:30:08 -0500

    Se realizan cambios para que la comunicacion sea mediante NATS

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -88,4 +88,26 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       }
     })
   }
+
+  async validateProduct(ids: number[]){
+    ids = Array.from(new Set(ids));
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    })
+
+    if (products.length !== ids.length ) {
+      throw new RpcException({
+        message: 'some products were not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
+  }
+
+
 }
